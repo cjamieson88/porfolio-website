1269b07e5dcddcf2bea673fa7052d4a2a8664e2b
cjamieson <chris.jamieson@gmail.com>
2023-01-25 12:52:21 -0800

    Add desc on new columns

--- main.py
+++ main.py
@@ -9,8 +9,11 @@ with col1:
 
 with col2:
     st.title("Chris Jamieson")
-    content = """Hi, I'm Chris! I am a project manager interested in the critical role quality data plays in 
+    content = """Hi, I'm Chris! I am a project manager exploring the critical role quality data plays in 
     manufacturing environments. I graduated in 2010 with a Bachelor of Science in Chemical Engineering from the 
     University of Washington. I have held roles at Janicki industries as the Director of Metrology and at Fluke 
     Corporation as an Operations Compliance Project Manager. """
     st.info(content)
+
+blurb = """Below you can find some of the apps I have built in Python, Feel free to contact me!"""
+st.write(blurb)
